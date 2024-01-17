fa645abf8f7173c9fbbe13a02d53f306edea7608
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-01-17 19:30:09 +0200

    del old func

--- js/task-2.js
+++ js/task-2.js
@@ -16,12 +16,12 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-      // return this.#items.reduce((accum, currItem) => {if(currItem !== itemToRemove) currItem)},[]);
       this.#items = this.#items.filter((item) => item !== itemToRemove);
   }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
